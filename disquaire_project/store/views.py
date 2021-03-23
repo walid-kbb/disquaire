cfbff5de36a412b1ecf8e321107accbb315fc3ea
Walid <walid-kebbab@outlook.fr>
2021-03-23 23:04:00 +0100

    new commit

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,13 +1,14 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Album, Artist, Booking, Contact
 
 def index(request):
     albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
     formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
-    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
-    return HttpResponse(message)
+    template=loader.get_template('store/index.html')
+    return HttpResponse(template.render(request=request))
 
 def listing(request):
     albums = Album.objects.filter(available=True)
